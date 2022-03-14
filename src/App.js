327971ba3856a99c1869cf31c72dd65fcd83bc98
Kaden Hansen <kadendeanhansen@gmail.com>
2022-03-14 11:01:21 -0400

    add colors to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { useState } from "react"
+import ColorBlock from "./ColorBlock"
 
 function App() {
+  let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock color={color} />
+    )
+  }) 
+
   return (
     <div className="App">
-
+      {colorMap}
     </div>
-  );
+  )
 }
 
 export default App;
